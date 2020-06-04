f1214dd81e0d86b630f792854a03a67a6a50a53b
Bjarne Kvæstad <bjarnekvae@gmail.com>
2020-06-04 23:53:27 +0200

    Add example to display price of cryptocurrency

---
+++ examples/crypto_price.py
@@ -0,0 +1,22 @@
+import serial
+import time
+import pymobitec_flipdot as flipdot
+import cryptocompare
+
+currency = 'ADA'
+
+with serial.Serial('/dev/ttyS0', 4800, timeout=1) as ser:
+
+    while True:
+        price = cryptocompare.get_price(currency, curr='USD')
+        price = price[currency]['USD']
+
+        if price > 1.0:
+            price_str = str(price)[:5]
+        else:
+            price_str = str(price)[1:6]
+
+        msg = flipdot.set_text(price_str, 1, 0, flipdot.Fonts.numbers_14px)
+        ser.write(msg)
+
+        time.sleep(60)
